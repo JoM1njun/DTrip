0383a7644f8d7c365f7c13a6d918b32c367e0a48
JoM1njun <brian2732@naver.com>
2025-12-10 23:52:49 +0900

    New Places Update

--- js/database/datainput.js
+++ js/database/datainput.js
@@ -12,9 +12,10 @@ async function insertMultipleDocuments() {
     // 👉 여러 데이터 준비
     // CategoryId: 1 = 카페, 2 = 식당, 3 = 문화, 4 = 체험 , 5 = 자연 , 6 = 미술관
     const items = [
-        { categoryId: 3, category: "문화", address: "대전광역시 서구 대덕대로 211", favorite: 333, id: 8, image_url: "assets/places/갤러리아.svg", lat: 36.352197, lng: 127.378227, name: "갤러리아백화점 타임월드점", phone: "1544-6600", rating: 4.4, review: 21630, time_start: "10:30", time_end: "20:00" },
-        { categoryId: 4, category: "체험", address: "대전광역시 서구 둔지로 14 102호, 2층", favorite: 72, id: 9, image_url: "assets/places/짱오락실.svg", lat: 36.351087, lng: 127.376292, name: "짱오락실 대전둔산점", phone: "0507-1413-7611", rating: 4.5, review: 131, time_start: "12:00", time_end: "24:00" },
-        { categoryId: 2, category: "식당", address: "대전광역시 서구 대덕대로249번길 15 1층 106호", favorite: 72, id: 9, image_url: "assets/places/타츠진.svg", lat: 36.354830, lng: 127.378108, name: "타츠진 우동", phone: "0507-1424-3020", rating: 4.3, review: 7580, time_start: "11:00", time_end: "20:00" },
+        { categoryId: "데이트", tag: "미술관", address: "대전 서구 둔산대로 155 둔산대공원", favorite: 333, id: 11, image_url: "assets/places/daejeonart.svg", lat: 36.367006, lng: 127.385827, name: "대전시립미술관", phone: "0507-1378-7370", rating: 4.57, review: 1286, time_start: "10:00", time_end: "18:00" },
+        { categoryId: "데이트", tag: "미술관", address: "대전 서구 둔산대로 157 이응노미술관", favorite: 213, id: 12, image_url: "assets/places/eungart.svg", lat: 36.367102, lng: 127.387051, name: "이응노미술관", phone: "0507-1490-9801", rating: 4.55, review: 385, time_start: "10:00", time_end: "18:00" },
+        { categoryId: "데이트", tag: "자연", address: "대전 동구 충정로 53 남간정사", favorite: 45, id: 13, image_url: "assets/places/uampark.svg", lat: 36.348006, lng: 127.456830, name: "우암사적공원", phone: "042-673-9286", rating: 4.5, review: 37, time_start: "05:00", time_end: "21:00" },
+        { categoryId: "데이트", tag: "자연", address: "대전 서구 장안로 461", favorite: 241, id: 14, image_url: "assets/places/jangtemountain.svg", lat: 36.218945, lng: 127.340221, name: "장태산자연휴양림", phone: "042-673-9286", rating: 4.44, review: 497, time_start: "09:00", time_end: "17:00" },
     ];
 
     // 👉 반복하며 batch.set()으로 문서 추가
